c81c20352ccdb11e4eaab2c6287684d2203fa4c5
connlost <hi.imhpc@outlook.com>
2023-09-20 19:43:58 -0500

    now accepting arbitrary numbers of file

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,45 @@ use std::{env, fs, path::Path, process::exit};
 
 use auto_bangumi_rs::BangumiInfo;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Usage: ./program path_to_file");
+    let mut args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: ./program file1 file2 file3...");
         exit(1);
     }
 
-    let path_str = &args[1];
-    let path = Path::new(path_str);
-    if !path.is_file() {
-        eprintln!("Not a file!");
-        exit(1);
-    }
+    args.remove(0);
+    for arg in args {
+        let path_str = &arg;
+        let path = Path::new(path_str);
+        if !path.is_file() {
+            eprintln!("Not a file!");
+            continue;
+        }
 
-    if let Some(filename) = path.file_name().and_then(|f| f.to_str()) {
-        let ext = match path.extension() {
-            Some(p) => p.to_str().unwrap(),
-            None => "",
-        };
+        if let Some(filename) = path.file_name().and_then(|f| f.to_str()) {
+            let ext = match path.extension() {
+                Some(p) => p.to_str().unwrap(),
+                None => "",
+            };
 
-        match BangumiInfo::parse(filename) {
-            Some(info) => {
-                let new_filename = info.gen_filename(ext);
-                println!("=> {}", new_filename);
-                let new_path = path_str.replace(filename, &new_filename);
+            match BangumiInfo::parse(filename) {
+                Some(info) => {
+                    let new_filename = sanitize_filename(&info.gen_filename(ext));
+                    println!("=> {}", new_filename);
+                    let new_path = path_str.replace(filename, &new_filename);
 
-                match rename_file(&path_str, &new_path) {
-                    Ok(_) => (),
-                    Err(e) => {
-                        eprintln!("Error: {}", e);
-                        exit(1);
+                    match rename_file(&path_str, &new_path) {
+                        Ok(_) => (),
+                        Err(e) => {
+                            eprintln!("Error: {}", e);
+                            continue;
+                        }
                     }
                 }
-            }
-            None => {
-                eprintln!("You found a bug, report to me.");
-                exit(1);
+                None => {
+                    eprintln!("You found a bug, report to me.");
+                    continue;
+                }
             }
         }
     }
@@ -45,4 +48,17 @@ fn main() {
 
 fn rename_file(src: &str, dst: &str) -> Result<(), std::io::Error> {
     fs::rename(src, dst)
+}
+
+fn sanitize_filename(filename: &str) -> String {
+    // Characters invalid on Windows: < > : " / \ | ? *
+    // Characters to avoid on Unix-like: / (as it's a directory separator) and null byte
+    // Combining both, we get the pattern: [<>:\"/\\|?*\0]
+
+    let sanitized: String = filename
+        .chars()
+        .filter(|&c| !"<>:\"/\\|?*\0".contains(c))
+        .collect();
+
+    sanitized
 }
